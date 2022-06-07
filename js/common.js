6902958f70c390c65991a7e37ba2e67a06e45129
shchocho <95754270+shchocho@users.noreply.github.com>
2022-06-07 17:46:14 +0900

    Last

--- js/common.js
+++ js/common.js
@@ -1,10 +1,16 @@
-// $(function(){
-//
-//   $("header").load("./header.html");
-//   $("#contents-wrap").load("./content.html");
-//
-//
-//
-//
-//
-// })
+$(function(){
+
+  $(".gvn-btn").click(function(){
+    if($(this).hasClass("data")){
+      $(".gvn-btn").removeClass("on");
+      $(this).addClass("on");
+      // $(".gvn-tbl").removeClass("active");
+      // $(".data").addClass("active");
+    }else if($(this).hasClass("onchain")){
+      $(".gvn-btn").removeClass("on");
+      $(this).addClass("on");
+      // $(".gvn-tbl").removeClass("active");
+      // $(".onchain").addClass("active");
+    }
+  })
+})
